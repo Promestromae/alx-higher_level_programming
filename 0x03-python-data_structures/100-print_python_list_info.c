77040abd294b555d153efb2671baad8f96be02d4
Promestromae <nzuvameshack@gmail.com>
2023-08-13 05:09:41 -0700

    sublime

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,7 +9,7 @@ void print_python_list_info(PyObject *p)
 	PyListObject *obj = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->alloacted);
+	printf("[*] Allocated = %li\n", obj->allocated);
 	for (y = 0; y < size; y++)
 		printf("Element %y: %s\n", y, Py_TYPE(obj->ob_item[y])->tp_name);
 }
