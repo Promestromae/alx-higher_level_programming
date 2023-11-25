dbbd1d538f8435113b363e11b74ad3d531d2af3c
Promestromae <nzuvameshack@gmail.com>
2023-11-25 07:02:01 -0800

    maestro

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Defines a function that converts a number from base 10 to another base
+exports.converter = function (base) {
+  // Return a function that takes a number as an argument
+  return function (number) {
+    // Convert the number from base 10 to the given base using the toString() method
+    return number.toString(base);
+  };
+};
