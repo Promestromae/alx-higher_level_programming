934941d36f5457a157338024f22942d10420c3e7
Promestromae <nzuvameshack@gmail.com>
2024-01-03 16:19:57 -0800

    void

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-const i = require('i');
-const file = process.argv[2];
-
-i.readFile(file, 'utf-8', function (err, data) {
+const filePath = process.argv[2];
+const fs = require('fs');
+fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
